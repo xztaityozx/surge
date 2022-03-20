e623d8ef8c85595a3c38e43a0d8a5364d01ae0f3
xztaityozx <taityonohibi@gmail.com>
2022-03-20 21:27:30 +0900

    test: テスト追加した

--- sub_process/src/lib.rs
+++ sub_process/src/lib.rs
@@ -1,9 +1,35 @@
 #[cfg(test)]
 mod tests {
+    use std::sync::Arc;
+
+    use crate::sub_process::SubProcessResult;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn it_error_msg() {
+        let cmd = &["a".to_string(), "b".to_string(), "c".to_string()];
+        let input = "this is input".as_bytes();
+        let output = "this is output".as_bytes();
+
+        let expected = [
+                "sub process exit code is not 0".to_string(),
+                "input:".to_string(),
+                format!("{}", String::from_utf8_lossy(input)),
+                "".to_owned(),
+                "command:".to_string(),
+                format!("\t{}", cmd.join(" ")),
+                "".to_owned(),
+                "output:".to_string(),
+                format!("\t{}", String::from_utf8_lossy(output)),
+        ].join("\n");
+
+        let sub_process_result = SubProcessResult{
+            cmd: Arc::new(cmd.to_vec()),
+            input: input.to_vec(),
+            output: output.to_vec(),
+            success: true
+        };
+
+        assert_eq!(expected, sub_process_result.error_msg())
     }
 }
 
