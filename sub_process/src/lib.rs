1fea0ae86b77ab3aa71200e2e9c7f27387345504
xztaityozx <taityonohibi@gmail.com>
2022-03-21 12:27:49 +0900

    test: テスト書いた

--- sub_process/src/lib.rs
+++ sub_process/src/lib.rs
@@ -1,25 +1,27 @@
 #[cfg(test)]
 mod tests {
-    use std::sync::Arc;
+    use std::{sync::Arc, thread::{self, JoinHandle}, process::{Command, Stdio}};
 
-    use crate::sub_process::SubProcessResult;
+    use crossbeam::channel::{Sender, Receiver};
+
+    use crate::sub_process::{SubProcessResult, SubProcess};
 
     #[test]
-    fn it_error_msg() {
+    fn it_sub_process_result_error_msg() {
         let cmd = &["a".to_string(), "b".to_string(), "c".to_string()];
         let input = "this is input".as_bytes();
         let output = "this is output".as_bytes();
 
         let expected = [
-                "sub process exit code is not 0".to_string(),
-                "input:".to_string(),
-                format!("{}", String::from_utf8_lossy(input)),
-                "".to_owned(),
-                "command:".to_string(),
-                format!("\t{}", cmd.join(" ")),
-                "".to_owned(),
-                "output:".to_string(),
-                format!("\t{}", String::from_utf8_lossy(output)),
+            "sub process exit code is not 0".to_string(),
+            "input:".to_string(),
+            format!("{}", String::from_utf8_lossy(input)),
+            "".to_owned(),
+            "command:".to_string(),
+            format!("\t{}", cmd.join(" ")),
+            "".to_owned(),
+            "output:".to_string(),
+            format!("\t{}", String::from_utf8_lossy(output)),
         ].join("\n");
 
         let sub_process_result = SubProcessResult{
@@ -30,6 +32,66 @@ mod tests {
         };
 
         assert_eq!(expected, sub_process_result.error_msg())
+    }
+    
+    #[test]
+    fn it_sub_process_spawn_ng() {
+        let cmd = Arc::new(["cargo", "--typo"].map(|s| s.to_string()).to_vec());
+        let (tx, rx): (Sender<JoinHandle<SubProcessResult>>, Receiver<JoinHandle<SubProcessResult>>) = crossbeam::channel::bounded(3);
+        let number_of_sub_process = 10;
+
+        let receiver = thread::spawn(move|| {
+            for h in rx {
+                let r = h.join().unwrap();
+                assert!(!r.success);
+            }
+        });
+
+        for _ in 1..number_of_sub_process {
+            let sp = SubProcess{
+                cmd: Arc::clone(&cmd),
+                tx: tx.clone(),
+                log_fatal: |_s| {std::process::exit(1)}
+            };
+
+            sp.spawn("".as_bytes().to_vec()).unwrap();
+        }
+
+        drop(tx);
+
+        receiver.join().unwrap();
+    }
+
+    #[test]
+    fn it_sub_process_spawn_ok() {
+        let cmd = Arc::new(["cargo", "--help"].map(|s| s.to_string()).to_vec());
+        let (tx, rx): (Sender<JoinHandle<SubProcessResult>>, Receiver<JoinHandle<SubProcessResult>>) = crossbeam::channel::bounded(3);
+        let number_of_sub_process = 10;
+
+        let expected = String::from_utf8_lossy(&Command::new("cargo").arg("--help").stdout(Stdio::piped()).spawn().unwrap().wait_with_output().unwrap().stdout).to_string();
+
+        let receiver = thread::spawn(move|| {
+            for h in rx {
+                let r = h.join().unwrap();
+                assert!(r.success);
+                let actual = String::from_utf8_lossy(&r.output).to_string();
+                assert_eq!(expected, actual);
+            }
+        });
+
+        for _ in 1..number_of_sub_process {
+            let sp = SubProcess{
+                cmd: Arc::clone(&cmd),
+                tx: tx.clone(),
+                log_fatal: |_s| {std::process::exit(1)}
+            };
+
+            sp.spawn("".as_bytes().to_vec()).unwrap();
+        }
+
+        drop(tx);
+
+        receiver.join().unwrap();
     }
 }
 
